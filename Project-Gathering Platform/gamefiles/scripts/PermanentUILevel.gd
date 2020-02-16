2c6741c373a366fc100a8ba0e595cdb925650048
Mikkel K <32062449+judo347@users.noreply.github.com>
2020-02-16 20:38:41 +0100

    GeneratedLevel: fixed permanentUI placement.

--- Project-Gathering Platform/gamefiles/scripts/PermanentUILevel.gd
+++ Project-Gathering Platform/gamefiles/scripts/PermanentUILevel.gd
@@ -4,6 +4,14 @@ var label_wood_value = null
 var label_stone_value = null
 var label_grass_value = null
 
+func _physics_process(delta):
+	
+	# Update UI location on level scene 
+	var playerX = get_tree().get_root().get_node("Level/Player").get_position().x
+	var cameraPosVec = get_parent().get_camera_position()
+	var uiHeight = 200
+	set_global_position(Vector2(cameraPosVec.x -OS.window_size.x, -OS.window_size.y + uiHeight))
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	label_grass_value = get_node("MarginContainer/HBoxContainer/InfoBars/BarGrass/Value")
